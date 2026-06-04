1d5aa44376417a2863e8c33d5c8c25fb1e0df2b9
付忠源 <2048920439@qq.com>
2026-06-04 17:21:12 +0800

    refactor(object): 提取 shallowEqual 函数至 utils 模块

--- src/service-core/react/use-observable.ts
+++ src/service-core/react/use-observable.ts
@@ -6,6 +6,8 @@ import { useSyncExternalStore } from "react";
 import type { Observable } from "rxjs";
 import { merge } from "rxjs";
 
+import { shallowEqual } from "@/shared/utils/object";
+
 function toSource<T>(observable: Observable<T> | Observable<T>[]): Observable<T> {
   return Array.isArray(observable) ? merge(...observable) : observable;
 }
@@ -19,58 +21,6 @@ export function useObservable<T>(observable: Observable<T> | Observable<T>[], ca
     return () => sub.unsubscribe();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
-}
-
-/**
- * 浅比较函数：比较两个对象的第一层属性
- * 对 Map/Set/Date 等内置类型做特判，避免 Object.keys 遗漏内部状态导致误判相等
- */
-function shallowEqual<T>(objA: T, objB: T): boolean {
-  if (Object.is(objA, objB)) return true;
-
-  if (typeof objA !== "object" || objA === null || typeof objB !== "object" || objB === null) {
-    return false;
-  }
-
-  // 类型不一致直接不等（Map vs Set, Date vs Object 等）
-  if (objA.constructor !== objB.constructor) return false;
-
-  // Map：逐个键值对 Object.is 比较
-  if (objA instanceof Map && objB instanceof Map) {
-    if (objA.size !== objB.size) return false;
-    for (const [key, val] of objA) {
-      if (!objB.has(key) || !Object.is(val, objB.get(key))) return false;
-    }
-    return true;
-  }
-
-  // Set：逐个元素 Object.is 比较
-  if (objA instanceof Set && objB instanceof Set) {
-    if (objA.size !== objB.size) return false;
-    for (const item of objA) {
-      if (!objB.has(item)) return false;
-    }
-    return true;
-  }
-
-  // Date：比较时间戳
-  if (objA instanceof Date && objB instanceof Date) {
-    return objA.getTime() === objB.getTime();
-  }
-
-  // 普通对象/数组：逐键 Object.is 比较
-  const keysA = Object.keys(objA);
-  const keysB = Object.keys(objB);
-
-  if (keysA.length !== keysB.length) return false;
-
-  for (const key of keysA) {
-    if (!Object.prototype.hasOwnProperty.call(objB, key) || !Object.is((objA as Record<string, unknown>)[key], (objB as Record<string, unknown>)[key])) {
-      return false;
-    }
-  }
-
-  return true;
 }
 
 /**
